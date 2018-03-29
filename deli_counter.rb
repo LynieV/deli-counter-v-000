6e55b84fb411ce0fe29f0d3f036d2575b7982066
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-29 17:03:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli = []
+    puts "The line is currently empty."
+  else katz_deli.each_with_index.join(" ") {|name, i|
+    "The line is currently: #{i+1}. #{name}"}
+  end  
+end
