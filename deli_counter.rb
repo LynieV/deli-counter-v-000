133716ec7c76dd66ce1a22c992aa67c000c1edd2
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-29 18:04:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name="")
-  puts "Welcome, #{name}. You are number #{i} in line."
+  puts "Welcome, #{name}. You are number #{i+1} in line."
 end
