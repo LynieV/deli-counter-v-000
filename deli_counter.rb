1317270527468a4cb93836b8bc126d9bbcd127e6
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-29 17:47:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     customers = katz_deli.each_with_index.map {|name, i| "#{i+1}. #{name}"}
-    puts "The line is currently: #{customers.join(" "}"
+    puts "The line is currently: #{customers.join(" ")}"
   end  
 end
