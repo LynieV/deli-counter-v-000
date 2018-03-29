30850bcc075a556c0d86ad73c2966026d2ad8e43
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-29 17:17:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli = []
     puts "The line is currently empty."
-  else katz_deli.each_with_index.map {|name, i|
+  else katz_deli.each_with_index.collect {|name, i|
     puts "The line is currently: #{i+1}. #{name}"}
   end  
 end
