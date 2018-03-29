8daf86d11186dcbda0452763ebca578827f329fb
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-29 17:24:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli = []
+  if katz_deli == []
     puts "The line is currently empty."
   else katz_deli.each_with_index.map.join(" ") {|name, i|
     puts "The line is currently: #{i+1}. #{name}"}
